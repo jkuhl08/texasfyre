634a2567bf2c2a3ee32501854d28c9d532a326f9
buggady <nicolasdeshefy@gmail.com>
2016-05-18 20:08:09 -0500

    defined var carousel

--- texasfyre/src/js/facebook.js
+++ texasfyre/src/js/facebook.js
@@ -87,7 +87,7 @@
 
   function getProfilePictures() {
     FB.api('/me/photos', 'GET', {"fields": "picture"}, 
-    
+    var carousel '';
     function(response) {
       for(var i = 0; i < 25; i++) {
         carousel += '<div class="item"><a class="lightbox" title="This is an image title" href="images/portfolio-1/1.jpg" data-lightbox-gallery="gallery2"><div class="thumb-overlay"><i class="fa fa-arrows-alt"></i></div><img src="' + response.data[i].picture + '" /></a></div>';
